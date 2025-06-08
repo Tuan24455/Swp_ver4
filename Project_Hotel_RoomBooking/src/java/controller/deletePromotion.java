a5ff510c5bde143fe915e90b04c764a7dccada07
tdlord04 <tdmai041218@gmail.com>
2025-06-08 18:35:49 +0700

    giao diện

--- Project_Hotel_RoomBooking/src/java/controller/deletePromotion.java
+++ Project_Hotel_RoomBooking/src/java/controller/deletePromotion.java
@@ -2,15 +2,10 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
  */
-<<<<<<< Updated upstream
 
-package controller;
+package controller.admin;
 
-=======
-package controller;
-
-import dao.RoomDao;
->>>>>>> Stashed changes
+import dao.PromotionDao;
 import java.io.IOException;
 import java.io.PrintWriter;
 import jakarta.servlet.ServletException;
@@ -18,58 +13,33 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-<<<<<<< Updated upstream
 
 /**
  *
  * @author Phạm Quốc Tuấn
  */
-@WebServlet(name="HomeServlet", urlPatterns={"/home"})
-public class HomeServlet extends HttpServlet {
+@WebServlet(name="deletePromotion", urlPatterns={"/deletePromotion"})
+public class deletePromotion extends HttpServlet {
    
     /** 
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
-=======
-import jakarta.servlet.http.HttpSession;
-import java.util.ArrayList;
-import java.util.List;
-import model.Room;
-import model.User;
-
-/**
- *
- * @author ADMIN
- */
-@WebServlet(name = "HomeServlet", urlPatterns = {"/home"})
-public class HomeServlet extends HttpServlet {
-
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
->>>>>>> Stashed changes
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-<<<<<<< Updated upstream
     throws ServletException, IOException {
-=======
-            throws ServletException, IOException {
->>>>>>> Stashed changes
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
             /* TODO output your page here. You may use following sample code. */
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
-<<<<<<< Updated upstream
-            out.println("<title>Servlet HomeServlet</title>");  
+            out.println("<title>Servlet deletePromotion</title>");  
             out.println("</head>");
             out.println("<body>");
-            out.println("<h1>Servlet HomeServlet at " + request.getContextPath () + "</h1>");
+            out.println("<h1>Servlet deletePromotion at " + request.getContextPath () + "</h1>");
             out.println("</body>");
             out.println("</html>");
         }
@@ -78,21 +48,6 @@ public class HomeServlet extends HttpServlet {
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /** 
      * Handles the HTTP <code>GET</code> method.
-=======
-            out.println("<title>Servlet HomeServlet</title>");
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet HomeServlet at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
-    }
-
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
->>>>>>> Stashed changes
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
@@ -100,26 +55,12 @@ public class HomeServlet extends HttpServlet {
      */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
-<<<<<<< Updated upstream
     throws ServletException, IOException {
         processRequest(request, response);
     } 
 
     /** 
      * Handles the HTTP <code>POST</code> method.
-=======
-            throws ServletException, IOException {
-        List<Room> roomlist = new ArrayList<>();
-        RoomDao dao = new RoomDao();
-        roomlist = dao.getAllRooms();
-        request.setAttribute("roomlist", roomlist);
-        request.getRequestDispatcher("home.jsp").forward(request, response);
-    }
-
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
->>>>>>> Stashed changes
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
@@ -127,22 +68,32 @@ public class HomeServlet extends HttpServlet {
      */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
-<<<<<<< Updated upstream
     throws ServletException, IOException {
-        processRequest(request, response);
+               String idStr = request.getParameter("id");
+        if (idStr != null) {
+            try {
+                int id = Integer.parseInt(idStr);
+                PromotionDao dao = new PromotionDao();
+                boolean deleted = dao.deletePromotion(id);
+                if (deleted) {
+                    
+                    response.sendRedirect(request.getContextPath() + "/promotionList");
+                } else {
+                    request.setAttribute("error", "Delete failed.");
+                    request.getRequestDispatcher("/admin/promotions.jsp").forward(request, response);
+                }
+            } catch (NumberFormatException e) {
+                e.printStackTrace();
+                request.setAttribute("error", "Invalid ID.");
+                request.getRequestDispatcher("admin/promotions.jsp").forward(request, response);
+            }
+        } else {
+            response.sendRedirect(request.getContextPath() + "/promotionList");
+        }
     }
 
     /** 
      * Returns a short description of the servlet.
-=======
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
-
-    /**
-     * Returns a short description of the servlet.
-     *
->>>>>>> Stashed changes
      * @return a String containing servlet description
      */
     @Override
