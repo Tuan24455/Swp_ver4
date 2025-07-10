bee62e4712f0ef406e8f63e9c7d40a0034d77293
kunhtml <phamxuanhieu04112000@gmail.com>
2025-07-10 15:35:46 +0700

    thêm review service

--- Project_Hotel_RoomBooking/src/java/controller/ServiceDetailServlet.java
+++ Project_Hotel_RoomBooking/src/java/controller/ServiceDetailServlet.java
@@ -1,6 +1,7 @@
 package controller;
 
 import dao.ServiceDao;
+import dao.ReviewDao;
 import java.io.IOException;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
@@ -9,6 +10,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.util.List;
 import model.Service;
+import model.ServiceReview;
 
 /**
  * Servlet kiểm soát trang chi tiết dịch vụ.
@@ -35,8 +37,13 @@ public class ServiceDetailServlet extends HttpServlet {
             }
             List<Service> related = dao.getRelatedServices(service.getServiceTypeId(), id, 6);
 
+            // Lấy danh sách review cho dịch vụ này
+            ReviewDao reviewDao = new ReviewDao();
+            List<ServiceReview> reviews = reviewDao.getServiceReviewsByServiceId(id);
+
             request.setAttribute("service", service);
             request.setAttribute("relatedServices", related);
+            request.setAttribute("reviews", reviews);
             request.getRequestDispatcher("serviceDetail.jsp").forward(request, response);
         } catch (NumberFormatException e) {
             response.sendRedirect("service");
